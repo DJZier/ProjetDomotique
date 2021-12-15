19bc7c8dbebdef6d0edee1978947e047e4f70b2c
Florian <flomarkspitz_64@hotmail.fr>
2021-12-15 10:21:03 +0100

    instanciate put instead of post

--- tempComparisonManagementMS/src/main/java/fr/insa/ebf/tempComparisonManagementMS/controller/TempComparisonRessource.java
+++ tempComparisonManagementMS/src/main/java/fr/insa/ebf/tempComparisonManagementMS/controller/TempComparisonRessource.java
@@ -1,5 +1,9 @@
 package fr.insa.ebf.tempComparisonManagementMS.controller;
 
+import java.net.URI;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -7,23 +11,35 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 public class TempComparisonRessource {
 	
+	private final String winMSURI = "http://localhost:8083/window/openWindow";
+	private final String tempExtMSURI = "http://localhost:8082/temperature/tempExt";
+	private final String tempIntMSURI = "http://localhost:8083/temperature/tempInt";
+	private final int idWin = 1;
+	private boolean status;
+	
 	@GetMapping("/comparisonResult")
 	public boolean comparisonResult() {
 		return true;
 	}
 
-	@GetMapping("/shouldWindowsOpen")
+	@GetMapping("/runShouldWindowsOpen")
 	public boolean getStateWindowst() {
+		
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+		
 		
 		// Instanciate of RestTemplate for Rest calls
 		RestTemplate restTemplate = new RestTemplate();
-		float tempExt = restTemplate.getForObject("http://localhost:8082/temperature/tempExt", float.class);
-		float tempInt = restTemplate.getForObject("http://localhost:8082/temperature/tempInt", float.class);
+		float tempExt = restTemplate.getForObject(tempExtMSURI, float.class);
+		float tempInt = restTemplate.getForObject(tempIntMSURI, float.class);
 		if ((tempInt < tempExt) && ((18 < tempExt) && (tempExt < 27))) {
-			return true;
+			status = true;
 		}
 		else {
-			return false;
-		}		
+			status = false;
+		}
+		restTemplate.put(winMSURI + "/" + idWin, status);
+		return status;
 	}
 }
