efe917ab3af0645b336ed7886e65474a789a96d9
Ewan Mackay <ewan.mackay@gaitup.com>
2021-12-15 11:54:08 +0100

    added shutter MS
     based on windows from sc1

---
+++ shuttersServiceMS/src/test/java/fr/insa/ebf/shuttersService/ShuttersServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.insa.ebf.shuttersService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShuttersServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
